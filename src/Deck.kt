6c1b93a215dd31a434634f3cef592bd8808b1b7d
mooracle <yanuar.prakosa@gmail.com>
2019-08-06 14:47:44 +0700

    Compacting the Deck

--- src/Deck.kt
+++ src/Deck.kt
@@ -1,17 +1,13 @@
 class Deck {
     //creating array of card and initializing it
     //in Kotlin initialization does not require keyword new
-    val cards: Array<Card> = Array(52, fun(i:Int): Card{
-        // the anonymous function will returns a Card object with cardValue, and suite
-        val value = i%13
-        val suite = when(i/13){
+    val cards: Array<Card> = Array(52) {Card(it%13, getSuite(it))}
+
+    private fun getSuite(i: Int) = when(i/13){
             //since i was declared as integer means the division will result in integer
             0 -> "Clubs"
             1 -> "Diamonds"
             2 -> "Hearts"
             else -> "Spades"
         }
-        //return the card object with parameter calculated at the anonymous function above
-        return Card(value, suite)
-    })
 }
