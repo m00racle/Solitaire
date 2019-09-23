b7626fd7f385852b02749a6ef8755c41b69b990a
mooracle <yanuar.prakosa@gmail.com>
2019-09-23 13:54:11 +0700

    Let the Games Begin

--- src/GameModel.kt
+++ src/GameModel.kt
@@ -8,4 +8,23 @@ class GameModel {
     val foundationPiles : Array<FoundationPile> = arrayOf(FoundationPile(clubs), FoundationPile(diamonds),
         FoundationPile(hearts), FoundationPile(spades))
     val tableauPiles = Array(7, { TableauPile() })
+
+    fun resetGame() {
+        /*
+        * this function is called in the first time start the game and when we reset the game
+        * it will clear the wastePile and foundationPile objects*/
+        //reset the wastePile
+        wastePile.clear()
+        //reset all piles inside the foundation pile
+        /*for (pile in foundationPiles){
+            pile.reset()
+        }*/
+        //use shorter version to clear each file in the foundationPiles immutable array
+        //Question: why here it is immutable? supposedly this can be changed later
+        foundationPiles.forEach {
+            //call it to represent each file inside foundationPiles, it only represent single
+            //object of pile which consist in the above for loop
+            it.reset()
+        }
+    }
 }
